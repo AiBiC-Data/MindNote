65ce488a6b6a713294a0af14052e3564a6894c5b
AiBiC-Data <whrnjsgh12@naver.com>
2021-08-18 11:10:25 +0900

    create validator

--- mindnote/diary/views.py
+++ mindnote/diary/views.py
@@ -15,11 +15,11 @@ def info(request):
     return render(request, 'diary/info.html')
 
 def page_create(request):
-    if request.method == 'POST': # 만약 요청 방식이 POST라면
-        form = PageForm(request.POST)  # 입력된 데이터와 폼을 합쳐서 바인딩 폼을 만듭니다.
-        new_page = form.save()  # 데이터 저장 및 생성된 데이터 모델 반환
-        return redirect('page-detail', page_id=new_page.id)
-    else:  # 만약 요청 방식이 GET이라면
-        form = PageForm()  # 새로운 form을 만들고 (빈 폼)
-        return render(request, 'diary/page_form.html', {'form': form})
-        # 템플릿으로 보내 렌더해서 결과로 돌려줍니다
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            new_page = form.save()
+            return redirect('page-detail', page_id=new_page.id)
+    else:
+        form = PageForm()
+    return render(request, 'diary/page_form.html', {'form': form})
