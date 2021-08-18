f7f7f9f8c516c9f5f14a2c6b34f4b52152171155
AiBiC-Data <whrnjsgh12@naver.com>
2021-08-18 10:48:21 +0900

    make writing page

--- mindnote/diary/views.py
+++ mindnote/diary/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Page
 from .forms import PageForm
 
@@ -15,5 +15,17 @@ def info(request):
     return render(request, 'diary/info.html')
 
 def page_create(request):
-    form = PageForm()
-    return render(request, 'diary/page_form.html', {'form': form})
+    if request.method == 'POST': # 만약 요청 방식이 POST라면
+        new_page = Page( # 입력된 데이터를 가져와서 Page 데이터 모델을 만들고
+            title=request.POST['title'],
+            content=request.POST['content'],
+            feeling=request.POST['feeling'],
+            score=request.POST['score'],
+            dt_created=request.POST['dt_created']
+        )
+        new_page.save() # 데이터베이스에 저장한 후
+        return redirect('page-detail', page_id=new_page.id)
+    else:  # 만약 요청 방식이 GET이라면
+        form = PageForm()  # 새로운 form을 만들고 (빈 폼)
+        return render(request, 'diary/page_form.html', {'form': form})
+        # 템플릿으로 보내 렌더해서 결과로 돌려줍니다
